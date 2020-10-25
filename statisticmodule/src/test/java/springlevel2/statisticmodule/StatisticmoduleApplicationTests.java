984fd6b5d02b342773e8845fbc97c09b551fbc3f
Polpsi <59441238+Polpsi@users.noreply.github.com>
2020-10-25 23:20:48 +0300

    Lesson6Before

---
+++ statisticmodule/src/test/java/springlevel2/statisticmodule/StatisticmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package springlevel2.statisticmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticmoduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
